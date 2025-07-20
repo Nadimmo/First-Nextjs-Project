c936477887973b7a4ad4fc0b55946a7d235c07d5
Nadim Mostofa <nadimmostafa334@gmail.com>
2025-07-20 15:57:23 +0600

    added all catch segment

---
+++ src/app/Categories/[...slug]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CategoriesDetails = ({params}) => {
+    console.log(params)
+  return (
+    <div>Details Page</div>
+  )
+}
+
+export default CategoriesDetails
---
+++ src/app/Categories/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CategoriesPage = () => {
+  return (
+    <div>soup, coffee , desert , salad</div>
+  )
+}
+
+export default CategoriesPage
